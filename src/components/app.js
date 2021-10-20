2b1a7b5318a32789ee2f844b5c00aa5275c0e679
William Fontanez <william.r.fontanez@gmail.com>
2021-10-20 17:14:26 -0500

    Modified app.js, portfolio-container.js, portfolio.items
    ADDED
    app.js: messed up and had to re-put source code
    
    portfolio-container: added map function of portfoilo-items
    
    portfolio-items: messed up and had to re put in source code

--- src/components/app.js
+++ src/components/app.js
@@ -4,11 +4,6 @@ import moment from "moment";
 import PortfolioContainer from "./portfolio/portfolio-container";
 
 export default class App extends Component {
-  constructor(){
-    super();
-    
-    console.log("Portfolio container has rendered");
-  }
   render() {
     return (
       <div className="app">
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
+
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
+  constructor() {
+    super();
+
+    console.log("Portfolio container has rendered");
+  }
+
+  portfolioItems() {
+    const data = ["Project A", "Project B", "Project C", "Project D"];
+
+    return data.map(item => {
+      return <PortfolioItem />;
+    });
+  }
+
   render() {
     return (
       <div>
         <h2>Portfolio items go here updated...</h2>
 
-        <PortfolioItem/>
+        {this.portfolioItems()}
       </div>
     );
   }
