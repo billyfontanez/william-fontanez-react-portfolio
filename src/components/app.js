c0b231f3a0edd2fea09213cb215cbebc8b1a6e08
William Fontanez <william.r.fontanez@gmail.com>
2021-10-20 16:23:13 -0500

    Changes: ReadME, app.js, and portfolio-container.js
    Removed:
    Started code - ReadMe and app
    
    Updated:
    Added Name - Read Me and App
    
    Created:
    Portfolio-Container to add portfolio items

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import moment from "moment"
+import React, { Component } from "react";
+import moment from "moment";
+
+import PortfolioContainer from "./portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>William Fontanezs Portfolio</h1>
-        {moment().format('MMMM Do YYYY, h:mm:ss a')}
+      <div className="app">
+        <h1>William Fontanez's Portfolio</h1>
+        <PortfolioContainer />
+        <PortfolioContainer />
+        <PortfolioContainer />
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
       </div>
     );
   }
