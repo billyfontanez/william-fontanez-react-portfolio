2a63de854bc7717430728465c0451becc5e71993
William Fontanez <william.r.fontanez@gmail.com>
2021-10-20 17:02:22 -0500

    Modified app.js, removed sample data, rendured a constructor

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import moment from "moment";
 
-import PortfolioContainer from "./portfolio-container";
+import PortfolioContainer from "./portfolio/portfolio-container";
 
 export default class App extends Component {
+  constructor(){
+    super();
+    
+    console.log("Portfolio container has rendered");
+  }
   render() {
     return (
       <div className="app">
         <h1>William Fontanez's Portfolio</h1>
-        <PortfolioContainer />
-        <PortfolioContainer />
-        <PortfolioContainer />
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+        <PortfolioContainer />
       </div>
     );
   }
