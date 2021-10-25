3a67fdff92a746819978f7b1bd733276a8f6b8b2
William Fontanez <william.r.fontanez@gmail.com>
2021-10-25 12:04:40 -0500

    Added Nav Links

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -6,24 +6,59 @@ export default class PortfolioContainer extends Component {
   constructor() {
     super();
 
-    console.log("Portfolio container has rendered");
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      data: [
+        {title: "Military Spouses Market", category: "eCommerce"}, 
+        {title: "Manage My Stuff", category: "Scheduling"},
+        {title: "Fontanez Inc.", category: "Enterprise"}, 
+        {title: "The Virtual Fitness Master", category: "Fitness"}
+      ]
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
   }
 
   portfolioItems() {
-    const data = ["Project A", "Project B", "Project C", "Project D"];
-
-    return data.map(item => {
-      return <PortfolioItem title={item} url={"google.com"} />;
+    return this.state.data.map(item => {
+      return <PortfolioItem title={item.title} url={"google.com"} />;
     });
   }
 
   render() {
     return (
       <div>
-        <h2>Portfolio items go here updated...</h2>
+        <h2>{this.state.pageTitle}</h2>
+
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
+        <button onClick={() => this.handleFilter("Fitness")}>
+          Fitness
+        </button>
+        <button onClick={() => this.handleFilter("eCommerce", "Scheduling", "Enterprise", "Fitness")}>
+          View
+        </button>
 
         {this.portfolioItems()}
       </div>
     );
   }
 }
+
+
+
