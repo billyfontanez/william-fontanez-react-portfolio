d6b0a987c964efafe92fd9beff698fe59e4237a6
William Fontanez <william.r.fontanez@gmail.com>
2021-10-27 16:01:51 -0500

    Added Navigation Styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {NavLink} from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 export default class NavigationComponent extends Component {
   constructor() {
@@ -8,22 +8,34 @@ export default class NavigationComponent extends Component {
 
   render() {
     return (
-      <div>
-        <NavLink exact to="/" activeClassName="nav-link-active">
-          Home
-        </NavLink>
+      <div className="nav-wrapper">
+        <div className="left-side">
+          <div className="nav-link-wrapper">
+            <NavLink exact to="/" activeClassName="nav-link-active">
+              Home
+            </NavLink>
+          </div>
 
-        <NavLink to="/about-me" activeClassName="nav-link-active">
-          About
-        </NavLink>
-        <NavLink to="/contact" activeClassName="nav-link-active">
-          Contact
-        </NavLink>
-        <NavLink to="/blog" activeClassName="nav-link-active">
-          Blog
-        </NavLink>
+          <div className="nav-link-wrapper">
+            <NavLink to="/about-me" activeClassName="nav-link-active">
+              About
+            </NavLink>
+          </div>
 
-        {false ? <button>Add Blog</button> : null}
+          <div className="nav-link-wrapper">
+            <NavLink to="/contact" activeClassName="nav-link-active">
+              Contact
+            </NavLink>
+          </div>
+
+          <div className="nav-link-wrapper">
+            <NavLink to="/blog" activeClassName="nav-link-active">
+              Blog
+            </NavLink>
+          </div>
+        </div>
+
+        <div className="right-side">Willaim Fontanez</div>
       </div>
     );
   }
