7891fccd27d9838aa56b36a626e0a5a8fd5754d3
William Fontanez <william.r.fontanez@gmail.com>
2021-11-01 10:01:04 -0500

    Updated no-match

--- src/components/pages/no-match.js
+++ src/components/pages/no-match.js
@@ -5,6 +5,7 @@ export default function() {
   return (
     <div>
       <h2>We couldn't find that page</h2>
+      <iframe src="https://giphy.com/embed/H54feNXf6i4eAQubud" width="480" height="405" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/chicken-bro-H54feNXf6i4eAQubud">via GIPHY</a></p>
       <Link to="/">Return to homepage</Link>
     </div>
   );
