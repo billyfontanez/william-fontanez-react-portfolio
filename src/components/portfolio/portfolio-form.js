dede540ef52328cb77e4928c471d53ed294a9440
William Fontanez <william.r.fontanez@gmail.com>
2021-11-01 14:19:08 -0500

    added styles to text in form

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -130,10 +130,7 @@ export default class PortfolioForm extends Component {
 
   render() {
     return (
-      <div>
-        <h1>PortfolioForm</h1>
-
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
           <div>
             <input
               type="text"
@@ -209,7 +206,6 @@ export default class PortfolioForm extends Component {
             <button type="submit">Save</button>
           </div>
         </form>
-      </div>
     );
   }
 }
