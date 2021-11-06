6415a8f13ea2cd6fb211265fa3a755f394567e0f
William Fontanez <william.r.fontanez@gmail.com>
2021-11-06 15:21:05 -0500

    added rich text editor

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import RichTextEditor from "../forms/rich-text-editor";
+
 export default class BlogForm extends Component {
   constructor(props) {
     super(props);
@@ -85,6 +87,10 @@ export default class BlogForm extends Component {
             <option value="Hide">Hide</option>
             <option value="Published">Published</option>
           </select>
+        </div>
+
+        <div className="one-column">
+          <RichTextEditor />
         </div>
         <button className="btn">Save</button>
       </form>
