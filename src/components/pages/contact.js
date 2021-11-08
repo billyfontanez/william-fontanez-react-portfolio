b4e203e1c73bbd2035b693b7dc8b501cf5e9a34c
William Fontanez <william.r.fontanez@gmail.com>
2021-11-08 09:24:05 -0600

    finished initial portfolio project

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,45 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPagePicture from "../../../static/assets/images/auth/login.png";
 
-export default function(){
-    return(
-        <div>
-            <h2>Contact</h2>
+export default function() {
+    return (
+        <div className="content-page-wrapper">
+        <div
+            className="left-column"
+            style={{
+            background: "url(" + contactPagePicture + ") no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center"
+            }}
+        />
+        <div className="right-column">
+            <div className="contact-bullet-points">
+            <div className="bullet-point-group">
+                <div className="icon">
+                <FontAwesomeIcon icon="phone" />
+                </div>
+
+                <div className="text">808-347-0035</div>
+            </div>
+
+            <div className="bullet-point-group">
+                <div className="icon">
+                <FontAwesomeIcon icon="envelope" />
+                </div>
+
+                <div className="text">william.r.fontanez@gmail.com</div>
+            </div>
+
+            <div className="bullet-point-group">
+                <div className="icon">
+                <FontAwesomeIcon icon="map-marked-alt" />
+                </div>
+
+                <div className="text">Scott AFB, IL</div>
+            </div>
+            </div>
         </div>
-    )
+        </div>
+    );
 }
