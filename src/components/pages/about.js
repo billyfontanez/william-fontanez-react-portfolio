3114546688b7a149dfef294c400a47b46b60d4c7
William Fontanez <william.r.fontanez@gmail.com>
2021-12-07 18:11:21 -0600

    Changed About section, made headshot fit and text larger

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,35 +1,23 @@
 import React from "react";
 import profilePicture from "../../../static/assets/images/bio/headshot.png";
 
+console.log(profilePicture);
+
 export default function() {
   return (
     <div className="content-page-wrapper">
-      <div
-        className="left-column"
-        style={{
-          background: "url(" + profilePicture + ") no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center"
-        }}
-      />
+      <div className="left-column">
+        <img src="../../../static/assets/images/bio/headshot.png" alt="William Fontanez Headshot" />
+      </div>
+
       <div className="right-column">
-        Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis
-        dapibus posuere velit aliquet. Sed posuere consectetur est at lobortis.
-        Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-        Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget
-        risus varius blandit sit amet non magna. Morbi leo risus, porta ac
-        consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida
-        at eget metus. Donec sed odio dui. Cras mattis consectetur purus sit
-        amet fermentum. Etiam porta sem malesuada magna mollis euismod. Nulla
-        vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque
-        ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non
-        commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
-        elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur
-        et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-        commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis
-        risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac
-        consectetur ac, vestibulum at eros. Vestibulum id ligula porta felis
-        euismod semper.
+      Hello Everyone! My name is William Fontanez, and here is a little about me; I have a Bachelor of Science in Health Sciences, 
+      Bachelor of Arts in Psychology, Certification of Competency in Full-stack Web Development, and many different types of training. 
+      Over the past 17 years, I have worked in many industries. Some of these were in Healthcare, Nonprofit, Construction, Computer, Food, 
+      Hospitality, and Communications. Also, for the past 13 years, I have been in the Army Reserves. For most of my career, I was an Engineer 
+      but recently made a career change to Civil Affairs. During those years, I have been a training supervisor, helped with the logistics of 
+      our equipment, a human resources supervisor, and am currently the unit's operations manager. Currently, I am a freelancer where most of 
+      my work is in Software Engineering. However, I do business consulting, create logos and business cards.
       </div>
     </div>
   );
